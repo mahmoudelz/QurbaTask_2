35ddd43d4c975143dbd0468d487c9542cc4fb212
mahmoudelz <mahmoudelz@hotmail.com>
2023-03-09 16:06:08 +0200

    Initial Commit

---
+++ QurbaTask/QurbaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  QurbaTaskApp.swift
+//  QurbaTask
+//
+//  Created by Mahmoud Mostafa on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct QurbaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
